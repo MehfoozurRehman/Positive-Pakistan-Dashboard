db01fa2ceab353ba66ae6f6871aac7264a2447f7
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-11-20 16:38:03 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import Category from "./Screens/Category";
+import Contact from "./Screens/Contact";
+import Dashboard from "./Screens/Dashboard";
+import Headline from "./Screens/Headline";
+import Login from "./Screens/Login";
+import News from "./Screens/News";
+import NewsSelection from "./Screens/NewsSelection";
+import Topics from "./Screens/Topics";
+
+function Main() {
+  return (
+    <div className="main">
+      <Header />
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+}
 
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Main />}>
+          <Route path="" element={<Dashboard />} />
+          <Route path="headline" element={<Headline />} />
+          <Route path="news" element={<News />} />
+          <Route path="news-selection" element={<NewsSelection />} />
+          <Route path="topics" element={<Topics />} />
+          <Route path="catagory" element={<Category />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
---
+++ src/Screens/NewsSelection.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewsSelection() {
+  return <div>NewsSelection</div>;
+}
