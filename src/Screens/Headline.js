e13975fcabf811287c0891b2c0cd0bed0f4f3ed3
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-11-21 16:15:45 +0500

    deployed

--- src/Screens/Headline.js
+++ src/Screens/Headline.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Headline() {
-  return <div>headline</div>;
+  return (
+    <div className="container">
+      <div className="container__heading">Headlines</div>
+      <div className="container__header"></div>
+      <div className="container__content">
+        <div className="container__content__table">
+          <div className="container__content__table__header">
+            <div className="container__content__table__header__entry"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
