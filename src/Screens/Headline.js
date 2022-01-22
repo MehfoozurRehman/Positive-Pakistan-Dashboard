76b5f5f430cb15ad98eef0aabfa21e55cf933029
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-01-22 22:33:33 +0500

    dropdown selector

--- src/Screens/Headline.js
+++ src/Screens/Headline.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import InputBox from "../Components/InputBox";
 import TableHeaderList from "../Components/TableHeaderList";
 import TableInfoList from "../Components/TableInfoList";
+import OutsideClickHandler from "react-outside-click-handler";
 
 export default function Headline() {
   const navigate = useNavigate();
@@ -26,6 +27,54 @@ export default function Headline() {
     { type: "btn" },
   ];
 
+  function StatusDropDown() {
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+    const [statusData, setStatusData] = useState("Published");
+    return (
+      <>
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setIsDropDownOpen(false);
+          }}
+        >
+          <div className="container__content__table__content__list__entry__status__wrapper">
+            <button
+              onClick={() => {
+                isDropDownOpen === false
+                  ? setIsDropDownOpen(true)
+                  : setIsDropDownOpen(false);
+              }}
+              className="container__content__table__content__list__entry__status"
+            >
+              {statusData}
+            </button>
+            {isDropDownOpen ? (
+              <div className="container__content__table__content__list__entry__status__selection">
+                <button
+                  className="container__content__table__content__list__entry__status__selection__entry"
+                  onClick={() => {
+                    setStatusData("Published");
+                    setIsDropDownOpen(false);
+                  }}
+                >
+                  Published
+                </button>
+                <button
+                  className="container__content__table__content__list__entry__status__selection__entry"
+                  onClick={() => {
+                    setStatusData("Un Published");
+                    setIsDropDownOpen(false);
+                  }}
+                >
+                  Un Published
+                </button>
+              </div>
+            ) : null}
+          </div>
+        </OutsideClickHandler>
+      </>
+    );
+  }
   return (
     <div className="container">
       <div className="container__heading">Headlines</div>
@@ -108,19 +157,7 @@ export default function Headline() {
                 John doe
               </div>
               <div className="container__content__table__content__list__entry">
-                <div className="container__content__table__content__list__entry__status__wrapper">
-                  <button className="container__content__table__content__list__entry__status">
-                    Published
-                  </button>
-                  <div className="container__content__table__content__list__entry__status__selection">
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Published
-                    </button>
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Un Published
-                    </button>
-                  </div>
-                </div>
+                <StatusDropDown />
               </div>
               <div className="container__content__table__content__list__entry">
                 12-10-2021
@@ -189,19 +226,7 @@ export default function Headline() {
                 John doe
               </div>
               <div className="container__content__table__content__list__entry">
-                <div className="container__content__table__content__list__entry__status__wrapper">
-                  <button className="container__content__table__content__list__entry__status">
-                    Published
-                  </button>
-                  <div className="container__content__table__content__list__entry__status__selection">
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Published
-                    </button>
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Un Published
-                    </button>
-                  </div>
-                </div>
+                <StatusDropDown />
               </div>
               <div className="container__content__table__content__list__entry">
                 12-10-2021
@@ -270,19 +295,7 @@ export default function Headline() {
                 John doe
               </div>
               <div className="container__content__table__content__list__entry">
-                <div className="container__content__table__content__list__entry__status__wrapper">
-                  <button className="container__content__table__content__list__entry__status">
-                    Published
-                  </button>
-                  <div className="container__content__table__content__list__entry__status__selection">
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Published
-                    </button>
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Un Published
-                    </button>
-                  </div>
-                </div>
+                <StatusDropDown />
               </div>
               <div className="container__content__table__content__list__entry">
                 12-10-2021
@@ -351,19 +364,7 @@ export default function Headline() {
                 John doe
               </div>
               <div className="container__content__table__content__list__entry">
-                <div className="container__content__table__content__list__entry__status__wrapper">
-                  <button className="container__content__table__content__list__entry__status">
-                    Published
-                  </button>
-                  <div className="container__content__table__content__list__entry__status__selection">
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Published
-                    </button>
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Un Published
-                    </button>
-                  </div>
-                </div>
+                <StatusDropDown />
               </div>
               <div className="container__content__table__content__list__entry">
                 12-10-2021
@@ -432,19 +433,145 @@ export default function Headline() {
                 John doe
               </div>
               <div className="container__content__table__content__list__entry">
-                <div className="container__content__table__content__list__entry__status__wrapper">
-                  <button className="container__content__table__content__list__entry__status">
-                    Published
-                  </button>
-                  <div className="container__content__table__content__list__entry__status__selection">
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Published
-                    </button>
-                    <button className="container__content__table__content__list__entry__status__selection__entry">
-                      Un Published
-                    </button>
-                  </div>
-                </div>
+                <StatusDropDown />
+              </div>
+              <div className="container__content__table__content__list__entry">
+                12-10-2021
+              </div>
+              <div className="container__content__table__content__list__entry">
+                12:30 - 9:05
+              </div>
+              <div className="container__content__table__content__list__entry">
+                <Link
+                  to="/dashboard/headline-edit"
+                  className="container__content__table__content__list__entry__btn"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-edit"
+                  >
+                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                  </svg>
+                </Link>
+                <button className="container__content__table__content__list__entry__btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 21.252 27.308"
+                  >
+                    <g
+                      id="Group_1038"
+                      data-name="Group 1038"
+                      transform="translate(-726.198 -43.964)"
+                    >
+                      <path
+                        id="Path_12068"
+                        data-name="Path 12068"
+                        d="M740.663,43.964c.433.516.945.956,1.406,1.444.081.086.181.058.274.058,1.608,0,3.216.006,4.824,0,.216,0,.286.043.282.271-.014.844-.012,1.688,0,2.532,0,.2-.044.264-.258.264-2.5-.008-4.993-.005-7.489-.005q-6.609,0-13.218.006c-.239,0-.289-.063-.286-.291q.022-1.252,0-2.5c0-.228.066-.272.282-.271,1.6.01,3.2,0,4.8.009a.51.51,0,0,0,.415-.175c.421-.453.9-.855,1.292-1.333Z"
+                        transform="translate(0)"
+                        fill="#ce0707"
+                      />
+                      <path
+                        id="Path_12069"
+                        data-name="Path 12069"
+                        d="M761.329,121.289c0,2.942.008,5.884,0,8.825a2.988,2.988,0,0,1-1.76,2.839,2.826,2.826,0,0,1-1.285.283q-6.066,0-12.131,0a3.035,3.035,0,0,1-3.042-2.909c-.006-.124-.005-.249-.005-.373q0-8.825-.007-17.65c0-.266.074-.313.323-.313q8.8.011,17.6,0c.251,0,.322.051.321.314C761.325,115.3,761.329,118.294,761.329,121.289Z"
+                        transform="translate(-15.392 -61.965)"
+                        fill="#ce0707"
+                      />
+                    </g>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="container__content__table__content__list">
+              <div
+                className="container__content__table__content__list__entry"
+                style={{ minWidth: "290px" }}
+              >
+                سندھ میں کیسز میں اضافہ ندھ میں کیسز میں اضافہ
+              </div>
+              <div className="container__content__table__content__list__entry">
+                John doe
+              </div>
+              <div className="container__content__table__content__list__entry">
+                <StatusDropDown />
+              </div>
+              <div className="container__content__table__content__list__entry">
+                12-10-2021
+              </div>
+              <div className="container__content__table__content__list__entry">
+                12:30 - 9:05
+              </div>
+              <div className="container__content__table__content__list__entry">
+                <Link
+                  to="/dashboard/headline-edit"
+                  className="container__content__table__content__list__entry__btn"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-edit"
+                  >
+                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                  </svg>
+                </Link>
+                <button className="container__content__table__content__list__entry__btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 21.252 27.308"
+                  >
+                    <g
+                      id="Group_1038"
+                      data-name="Group 1038"
+                      transform="translate(-726.198 -43.964)"
+                    >
+                      <path
+                        id="Path_12068"
+                        data-name="Path 12068"
+                        d="M740.663,43.964c.433.516.945.956,1.406,1.444.081.086.181.058.274.058,1.608,0,3.216.006,4.824,0,.216,0,.286.043.282.271-.014.844-.012,1.688,0,2.532,0,.2-.044.264-.258.264-2.5-.008-4.993-.005-7.489-.005q-6.609,0-13.218.006c-.239,0-.289-.063-.286-.291q.022-1.252,0-2.5c0-.228.066-.272.282-.271,1.6.01,3.2,0,4.8.009a.51.51,0,0,0,.415-.175c.421-.453.9-.855,1.292-1.333Z"
+                        transform="translate(0)"
+                        fill="#ce0707"
+                      />
+                      <path
+                        id="Path_12069"
+                        data-name="Path 12069"
+                        d="M761.329,121.289c0,2.942.008,5.884,0,8.825a2.988,2.988,0,0,1-1.76,2.839,2.826,2.826,0,0,1-1.285.283q-6.066,0-12.131,0a3.035,3.035,0,0,1-3.042-2.909c-.006-.124-.005-.249-.005-.373q0-8.825-.007-17.65c0-.266.074-.313.323-.313q8.8.011,17.6,0c.251,0,.322.051.321.314C761.325,115.3,761.329,118.294,761.329,121.289Z"
+                        transform="translate(-15.392 -61.965)"
+                        fill="#ce0707"
+                      />
+                    </g>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="container__content__table__content__list">
+              <div
+                className="container__content__table__content__list__entry"
+                style={{ minWidth: "290px" }}
+              >
+                سندھ میں کیسز میں اضافہ ندھ میں کیسز میں اضافہ
+              </div>
+              <div className="container__content__table__content__list__entry">
+                John doe
+              </div>
+              <div className="container__content__table__content__list__entry">
+                <StatusDropDown />
               </div>
               <div className="container__content__table__content__list__entry">
                 12-10-2021
